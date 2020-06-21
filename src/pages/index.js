c2478173c86cd20cdeea0ed779b8b67231647efc
Keisuke Ashizaki <ashizaki_keisuke@mbpsoft.co.jp>
2020-06-21 22:11:39 +0900

    modify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,46 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <header className="header">
+        <div className="container">
+          <div className="site">
+            <a href="index.html">
+              <img src="images/logo.svg" alt="ESSENTIALS" />
+            </a>
+          </div>
+          <ul>
+            <li>TOP</li>
+            <li>ABOUT</li>
+          </ul>
+        </div>
+      </header>
+
+      <section className="hero">
+        <h1>
+          There is no love sincerer that
+          <br />
+          the love of food.
+        </h1>
+      </section>
+
+      <section className="food"></section>
+
+      <section className="photo"></section>
+
+      <footer className="footer">
+        <div className="container">
+          <ul className="sns">
+            <li>
+              <a href="https://twitter.com/">
+                <i className="fab fa-twitter" />
+                <span className="sr-only">Twitter</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  )
 }
