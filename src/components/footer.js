515c1df275d027e741e40bb7581ca7b391bce09c
Keisuke Ashizaki <ashizaki_keisuke@mbpsoft.co.jp>
2020-06-24 16:07:33 +0900

    modify footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,13 @@ import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faFacebookSquare,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
+
 export default () => {
   const data = useStaticQuery(graphql`
     query {
@@ -38,19 +45,19 @@ export default () => {
         <ul className="sns">
           <li>
             <a href="https://twitter.com/">
-              <i className="fab fa-twitter" />
+              <FontAwesomeIcon icon={faTwitter} />
               <span className="sr-only">Twitter</span>
             </a>
           </li>
           <li>
             <a href="https://facebook.com/">
-              <i className="fab fa-facebook-square" />
+              <FontAwesomeIcon icon={faFacebookSquare} />
               <span className="sr-only">Facebook</span>
             </a>
           </li>
           <li>
             <a href="http://instagram.com/">
-              <i className="fab fa-instagram" />
+              <FontAwesomeIcon icon={faInstagram} />
               <span className="sr-only">Instagram</span>
             </a>
           </li>
